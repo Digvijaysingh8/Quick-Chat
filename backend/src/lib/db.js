42493994037353f7c52627123e8c57a0b4f5174b
Digvijaysingh8 <128850850+Digvijaysingh8@users.noreply.github.com>
2025-03-15 23:29:04 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,10 +6,7 @@ export const connectDB = async () => {
   try {
     const x=process.env.MONGODB_URI;
     console.log(x);
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
